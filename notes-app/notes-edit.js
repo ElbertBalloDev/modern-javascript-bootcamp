1ce45d55a786f5c8967cf8824ca9b60dd01d1a43
Elbert Ballo <eballofullstackdev@gmail.com>
2020-07-08 22:27:25 -0700

    localstorage syncing across pages

--- notes-app/notes-edit.js
+++ notes-app/notes-edit.js
@@ -3,10 +3,9 @@ const bodyElement = document.querySelector("#note-body");
 const removeElement = document.querySelector("#remove-note");
 const goBack = document.querySelector("#go-back");
 const noteId = location.hash.substring(1);
-const notes = getSavedNotes();
-const note = notes.find(note => note.id === noteId);
-console.log(notes);
-console.log(note);
+
+let notes = getSavedNotes();
+let note = notes.find(note => note.id === noteId);
 note === undefined && location.assign("/");
 
 titleElement.value = note.title;
@@ -23,11 +22,22 @@ bodyElement.addEventListener("input", e => {
 });
 
 removeElement.addEventListener("click", e => {
-  removeNote(note.id);
+	removeNote(note.id);
 	saveNotes(notes);
 	location.assign("/");
 });
 
 goBack.addEventListener("click", () => {
 	location.assign("/");
+});
+
+window.addEventListener("storage", e => {
+	if (e.key === "notes") {
+		notes = JSON.parse(e.newValue);
+		note = notes.find(note => note.id === noteId);
+		note === undefined && location.assign("/");
+
+		titleElement.value = note.title;
+		bodyElement.value = note.body;
+	}
 });
